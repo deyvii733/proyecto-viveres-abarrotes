8d7b05c252b5da997df599dfa2ba4c939b8d0e3b
Cesar Jimenez <cesardjimeenz@unicesar.edu.co>
2026-09-18 07:59:07 -0500

    Actualiza cache de la aplicacion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mercado-norte-v2';
+const CACHE_NAME = 'mercado-norte-v3';
 const APP_FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
